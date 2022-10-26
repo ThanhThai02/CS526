694682fd108978b36b3230cd57880acc23893670
ThanhThai02 <20520305@gm.com>
2022-10-26 14:51:52 +0700

    eval

---
+++ App.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Screen from './components/Screen';
+
+export default class App extends React.Component {
+  render() {
+    return <Screen />;
+  }
+}
